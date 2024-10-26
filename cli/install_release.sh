4b11a569fbad87b4a9f452ae8964e5cf40ef6536
Jared Henderson <34703139+jaredhenderson@users.noreply.github.com>
2024-10-26 17:23:55 +1100

    Customisations for Batocera 40 install

--- cli/install_release.sh
+++ cli/install_release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+# [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 
 # check if JQ is installed
 if ! command -v jq &> /dev/null
@@ -19,16 +19,18 @@ fi
 
 echo "Installing Steam Deck Plugin Loader release..."
 
-USER_DIR="$(getent passwd $SUDO_USER | cut -d: -f6)"
+USER_DIR="/userdata/system"
 HOMEBREW_FOLDER="${USER_DIR}/homebrew"
+SERVICES_FOLDER="/userdata/system/services"
+FLATPAK_FOLDER="/userdata/saves/flatpak/data"
 
 # Create folder structure
 rm -rf "${HOMEBREW_FOLDER}/services"
-sudo -u $SUDO_USER mkdir -p "${HOMEBREW_FOLDER}/services"
-sudo -u $SUDO_USER mkdir -p "${HOMEBREW_FOLDER}/plugins"
-sudo -u $SUDO_USER touch "${USER_DIR}/.steam/steam/.cef-enable-remote-debugging"
+mkdir -p "${HOMEBREW_FOLDER}/services"
+mkdir -p "${HOMEBREW_FOLDER}/plugins"
+
 # if installed as flatpak, put .cef-enable-remote-debugging there
-[ -d "${USER_DIR}/.var/app/com.valvesoftware.Steam/data/Steam/" ] && sudo -u $SUDO_USER touch "${USER_DIR}/.var/app/com.valvesoftware.Steam/data/Steam/.cef-enable-remote-debugging"
+touch "${FLATPAK_FOLDER}/.var/app/com.valvesoftware.Steam/data/Steam/.cef-enable-remote-debugging"
 
 # Download latest release and install it
 RELEASE=$(curl -s 'https://api.github.com/repos/SteamDeckHomebrew/decky-loader/releases' | jq -r "first(.[] | select(.prerelease == "false"))")
@@ -39,51 +41,29 @@ printf "Installing version %s...\n" "${VERSION}"
 curl -L $DOWNLOADURL --output ${HOMEBREW_FOLDER}/services/PluginLoader
 chmod +x ${HOMEBREW_FOLDER}/services/PluginLoader
 
-echo "Check for SELinux presence and if it is present, set the correct permission on the binary file..."
-hash getenforce 2>/dev/null && getenforce | grep "Enforcing" >/dev/null && chcon -t bin_t ${HOMEBREW_FOLDER}/services/PluginLoader
+# echo "Check for SELinux presence and if it is present, set the correct permission on the binary file..."
+# hash getenforce 2>/dev/null && getenforce | grep "Enforcing" >/dev/null && chcon -t bin_t ${HOMEBREW_FOLDER}/services/PluginLoader
 
 echo $VERSION > ${HOMEBREW_FOLDER}/services/.loader.version
 
-systemctl --user stop plugin_loader 2> /dev/null
-systemctl --user disable plugin_loader 2> /dev/null
+batocera-services stop plugin_loader 2> /dev/null
+batocera-services disable plugin_loader 2> /dev/null
 
-systemctl stop plugin_loader 2> /dev/null
-systemctl disable plugin_loader 2> /dev/null
+curl -L https://raw.githubusercontent.com/jaredhenderson/decky-loader/main/dist/plugin_loader-release.sh  --output ${SERVICES_FOLDER}/plugin_loader-release
 
-curl -L https://raw.githubusercontent.com/SteamDeckHomebrew/decky-loader/main/dist/plugin_loader-release.service  --output ${HOMEBREW_FOLDER}/services/plugin_loader-release.service
-
-cat > "${HOMEBREW_FOLDER}/services/plugin_loader-backup.service" <<- EOM
-[Unit]
-Description=SteamDeck Plugin Loader
-After=network.target
-[Service]
-Type=simple
-User=root
-Restart=always
-ExecStart=${HOMEBREW_FOLDER}/services/PluginLoader
-WorkingDirectory=${HOMEBREW_FOLDER}/services
-Environment=UNPRIVILEGED_PATH=${HOMEBREW_FOLDER}
-Environment=PRIVILEGED_PATH=${HOMEBREW_FOLDER}
-Environment=LOG_LEVEL=INFO
-[Install]
-WantedBy=multi-user.target
-EOM
-
-if [[ -f "${HOMEBREW_FOLDER}/services/plugin_loader-release.service" ]]; then
+if [[ -f "${SERVICES_FOLDER}/plugin_loader-release" ]]; then
     printf "Grabbed latest release service.\n"
-    sed -i -e "s|\${HOMEBREW_FOLDER}|${HOMEBREW_FOLDER}|" "${HOMEBREW_FOLDER}/services/plugin_loader-release.service"
-    cp -f "${HOMEBREW_FOLDER}/services/plugin_loader-release.service" "/etc/systemd/system/plugin_loader.service"
+    # sed -i -e "s|\${SERVICES_FOLDER}|${SERVICES_FOLDER}|" "${SERVICES_FOLDER}/plugin_loader-release.service"
+    cp -f "${SERVICES_FOLDER}/plugin_loader-release.service" "${SERVICES_FOLDER}/plugin_loader"
 else
     printf "Could not curl latest release systemd service, using built-in service as a backup!\n"
-    rm -f "/etc/systemd/system/plugin_loader.service"
-    cp "${HOMEBREW_FOLDER}/services/plugin_loader-backup.service" "/etc/systemd/system/plugin_loader.service"
+    cp "${SERVICES_FOLDER}/plugin_loader-backup" "${SERVICES_FOLDER}/plugin_loader"
 fi
 
-mkdir -p ${HOMEBREW_FOLDER}/services/.systemd
-cp ${HOMEBREW_FOLDER}/services/plugin_loader-release.service ${HOMEBREW_FOLDER}/services/.systemd/plugin_loader-release.service
-cp ${HOMEBREW_FOLDER}/services/plugin_loader-backup.service ${HOMEBREW_FOLDER}/services/.systemd/plugin_loader-backup.service
-rm ${HOMEBREW_FOLDER}/services/plugin_loader-backup.service ${HOMEBREW_FOLDER}/services/plugin_loader-release.service
+mkdir -p ${SERVICES_FOLDER}/services/.systemd
+cp ${SERVICES_FOLDER}/services/plugin_loader-release.service ${SERVICES_FOLDER}/services/.systemd/plugin_loader-release.service
+cp ${SERVICES_FOLDER}/services/plugin_loader-backup.service ${SERVICES_FOLDER}/services/.systemd/plugin_loader-backup.service
+rm ${SERVICES_FOLDER}/services/plugin_loader-backup.service ${SERVICES_FOLDER}/services/plugin_loader-release.service
 
-systemctl daemon-reload
-systemctl start plugin_loader
-systemctl enable plugin_loader
+batocera-services enable plugin_loader
+batocera-services start plugin_loader
